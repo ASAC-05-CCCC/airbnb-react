313ceeeda7f88ff9b2e73deab59877ae92d82aab
IforCU <tmdgus0844@gmail.com>
2024-05-17 17:40:18 +0900

    Remove : Delete src/components/Comment

--- src/components/Comment/GuestFavorite.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const GuestFavorite = () => {
-  return <div>GuestFavorite</div>
-}
-
-export default GuestFavorite
--- src/components/Comment/ReviewMeta.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ReviewMeta = () => {
-  return <div>ReviewMeta</div>
-}
-
-export default ReviewMeta
