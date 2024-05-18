aed49190b858c75aeefb559d5dfff698baeb077a
IforCU <tmdgus0844@gmail.com>
2024-05-18 13:32:38 +0900

    Feat : page Notification.jsx

---
+++ src/pages/Notification/Notification.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NoticeHeader from '../../components/NoticeHeader/NoticeHeader.jsx'
+import NoticeContents from '../../components/NoticeContents/NoticeContents.jsx'
+
+const Notification = () => {
+  return (
+    <div>
+      <NoticeHeader />
+      <NoticeContents />
+    </div>
+  )
+}
+
+export default Notification
