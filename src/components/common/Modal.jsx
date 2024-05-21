d024b4dca2ab649a319c507314512588bcb07ac8
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-05-21 15:17:32 +0900

    Feat: Make Modal component

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,9 @@
+const Modal = ({ children }) => {
+  return (
+    <>
+      <div className='w-full border border-gray-100 shadow-md rounded-xl'>{children}</div>
+    </>
+  )
+}
+
+export default Modal
