4aeabfaf24179ca924f4c0b7d15a62f0ff39fe4d
IforCU <tmdgus0844@gmail.com>
2024-05-21 10:15:12 +0900

    Feat : ReveiewMeta add Content, Overall

--- src/components/ReviewMeta/ReviewMeta.jsx
+++ src/components/ReviewMeta/ReviewMeta.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import ReviewMetaContent from './ReviewMetaContent'
+import ReviewOverall from './ReviewOverall'
 
 const ReviewMeta = () => {
-  return <div>ReviewMeta</div>
+  return (
+    <div>
+      <ReviewOverall />
+      <ReviewMetaContent />
+    </div>
+  )
 }
 
 export default ReviewMeta
