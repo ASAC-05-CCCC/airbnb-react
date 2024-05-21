db4ee7ecaf3b87c09c4c9d840fa382afd252f7f5
yunhyungNAM <iys04003@naver.com>
2024-05-21 14:44:13 +0900

    Feat: Component RoomIntroduce.jsx

---
+++ src/components/RoomIntroduce/RoomIntroduce.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Translation from './Translation.jsx'
+import IntroduceContent from './IntroduceContent.jsx'
+
+const RoomIntroduce = () => {
+  return (
+    <div>
+      <Translation />
+      <IntroduceContent />
+    </div>
+  )
+}
+
+export default RoomIntroduce
