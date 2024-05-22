087641012fa8c9c55a825870c04d3e508df2c17d
IforCU <tmdgus0844@gmail.com>
2024-05-22 13:36:58 +0900

    Fix : 썸네일을 좀 더 자연스럽게 바꿈

--- src/components/Review/ReviewHeader.jsx
+++ src/components/Review/ReviewHeader.jsx
@@ -1,9 +1,9 @@
 const ReviewHeader = ({ image, name, nationality }) => (
   <div className='flex items-center'>
-    <img src={image} alt={name} className='w-10 h-10 rounded-full mr-4' />
+    <img src={image} alt={name} className='w-10 h-10 bg-neutral-200 rounded-full mr-4' />
     <div>
       <p className='font-bold'>{name}</p>
-      <p className='text-sm text-gray-600'>{nationality}</p>
+      <p className='text-sm font-normal text-neutral-800'>{nationality}</p>
     </div>
   </div>
 )
