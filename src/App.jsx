8099ba86d61c9e3a7ad679c119f5e3cc8c73660c
IforCU <tmdgus0844@gmail.com>
2024-05-22 18:31:24 +0900

    Fix : develop를 위해 app을 약간 바꿈

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Notification from './components/Notification/Notification.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div></div>
+      <div>
+        <Notification />
+      </div>
     </>
   )
 }
