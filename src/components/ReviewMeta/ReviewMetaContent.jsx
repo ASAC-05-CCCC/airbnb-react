d11fe96afa9fe1302dad4a1dfdb1641d3e3c7c01
IforCU <tmdgus0844@gmail.com>
2024-05-22 13:37:36 +0900

    Fix : 각 요소 위치 조정함

--- src/components/ReviewMeta/ReviewMetaContent.jsx
+++ src/components/ReviewMeta/ReviewMetaContent.jsx
@@ -2,7 +2,7 @@ function ReviewMetaContent({ category, value, icon }) {
   return (
     <div className='flex font-semibold bg-white p-4 '>
       <div className='border-l-2 border-gray-400 mr-5'></div> {/* 세로 선 */}
-      <div className='flex-col text-sm'>
+      <div className='flex-col text-sm '>
         <div className='text-gray-700'>{category}</div>
         <div className='flex-col'>
           <div className='text-gray-700 mb-5'>{value}</div>
