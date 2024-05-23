e49154cde17a41ff489d76babc37f848f4ee2eab
jueun0725 <kimjueun010@naver.com>
2024-05-23 16:42:20 +0900

    Feat: 장소리스트 데이터 추가, Swiper 추가

--- src/components/PlaceList/PlaceCardImageList.jsx
+++ src/components/PlaceList/PlaceCardImageList.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 // 장소 이미지 슬라이더 리스트 (사진 넘기는거)
 const PlaceCardImageList = ({ photos }) => {
-  return <div></div>
-}
+  // price에서 숫자만 추출하고 정수로 변환
 
+  return (
+    <div className='relative w-[303px] h-[287px] group'>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true} //화살표 커스텀
+        modules={[Pagination, Navigation]}
+        className='mySwiper'
+      >
+        {photos.map((photo, index) => (
+          <SwiperSlide key={index}>
+            <div className='relative w-full h-full'>
+              <img
+                src={photo}
+                alt={`Slide`}
+                className='w-[303px] h-[287px] bg-zinc-400 rounded-xl'
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
 export default PlaceCardImageList
