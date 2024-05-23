c197d2a94d694cbcd3608a297659d877db37ca98
IforCU <tmdgus0844@gmail.com>
2024-05-23 17:13:28 +0900

    Fix: Reveiew 6개로 제한해서 comment를 보여줌

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -35,6 +35,54 @@ const commentData = [
     timeStamp: '5 hours ago',
     message: '잘가세요.',
   },
+  {
+    image: './no',
+    name: 'Earon',
+    nationality: 'Chinese',
+    rating: 4.6,
+    timeStamp: '6 hours ago',
+    message: '좋은 하루 되세요.',
+  },
+  {
+    image: './no',
+    name: 'Faron',
+    nationality: 'German',
+    rating: 4.2,
+    timeStamp: '7 hours ago',
+    message: '안녕히 계세요.',
+  },
+  {
+    image: './no',
+    name: 'Garon',
+    nationality: 'Brazilian',
+    rating: 4.4,
+    timeStamp: '8 hours ago',
+    message: '감사합니다.',
+  },
+  {
+    image: './no',
+    name: 'Haron',
+    nationality: 'Canadian',
+    rating: 4.9,
+    timeStamp: '9 hours ago',
+    message: '좋습니다!',
+  },
+  {
+    image: './no',
+    name: 'Iaron',
+    nationality: 'Russian',
+    rating: 3.8,
+    timeStamp: '10 hours ago',
+    message: '별로입니다.',
+  },
+  {
+    image: './no',
+    name: 'Jaron',
+    nationality: 'Indian',
+    rating: 4.3,
+    timeStamp: '11 hours ago',
+    message: '환영합니다.',
+  },
 ]
 
 const Review = () => {
@@ -43,6 +91,7 @@ const Review = () => {
       <hr className='my-4 border-gray-300' />
       <div className='grid grid-cols-1 lg:grid-cols-2 w-full '>
         {commentData.map((comment, index) => {
+          if (index > 5) return null
           return (
             <div key={index} className='bg-white p-4 px-2 mr-20'>
               <div className=' mb-10 text-base'>
