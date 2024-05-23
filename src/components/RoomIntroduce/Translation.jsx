26ff9dbc1622bb61e0dec184389e4753755632eb
yunhyungNAM <iys04003@naver.com>
2024-05-23 19:11:03 +0900

    Feat: Translation.jsx

--- src/components/RoomIntroduce/Translation.jsx
+++ src/components/RoomIntroduce/Translation.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Translation = () => {
-  return <div>Translation</div>
+  return (
+    <div className="max-w-2xl mx-auto p-4 border-t border-gray-300">
+      <div className='flex space-x-2 border items-end'>
+        <div>일부 정보는 자동 번역되었습니다.</div>
+        <button className='underline'>원문 보기</button>
+      </div>
+      
+    </div>
+  )
 }
 
 export default Translation
