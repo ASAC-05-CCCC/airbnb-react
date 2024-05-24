ad881fc2f717873f107a6bada51eb10f9039022d
ji-mango <susie000301@naver.com>
2024-05-24 16:59:24 +0900

    Feat: 캐러셀 구현, 카테고리 요소 hover 구현

--- src/components/MainCategory/Category.jsx
+++ src/components/MainCategory/Category.jsx
@@ -1,35 +1,78 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { useState, useRef } from 'react'
 
 import CategoryItem from './CategoryItem'
 import FilterButton from './FilterButton'
-import DetailModal from '../common/DetailModal'
 
-import categoryData from '../../../public/json/categoryList.json'
+import categoryData from '../../../public/json/categoryList.json'
 import FilterComponent from './FilterComponent'
+import ArrowButton from '../common/ArrowButton'
 
 function Category() {
   const categoryList = categoryData.categoryList
 
   const [isFilterModal, setIsFilterModal] = useState(false)
+  const [isLeftArrow, setIsLeftArrow] = useState(true)
+  const [isRightArrow, setIsRightArrow] = useState(false)
+  const [transX, setTransX] = useState(0)
+
+  const onClickLeftArrowButton = () => {
+    console.log(transX)
+    if (transX === -200) {
+      setTransX(transX - 60)
+      setIsLeftArrow(false)
+    } else {
+      setTransX(transX - 50)
+      setIsRightArrow(true)
+    }
+  }
+
+  const onClickRightArrowButton = () => {
+    console.log(transX)
+    if (transX === -60) {
+      setTransX(transX + 60)
+      setIsRightArrow(false)
+    } else if (transX === -50) {
+      setTransX(transX + 50)
+      setIsRightArrow(false)
+    } else {
+      setTransX(transX + 50)
+      setIsLeftArrow(true)
+    }
+  }
 
   return (
-    <>
-      <div className='flex flex-row place-items-center place-content-center'>
-        <div className='flex flex-row space-x-5 p-5 overflow-auto w-10/12'>
-          {categoryList.map((item, index) => {
-            return (
-              <CategoryItem
-                key={index}
-                categoryIcon={item.categoryIcon}
-                categoryName={item.categoryName}
-              />
-            )
-          })}
+    <div className='flex flex-row place-items-center place-content-center'>
+      <div className='flex flex-row place-items-center place-content-center w-[95%]'>
+        <div className='w-10 h-10 flex place-items-center place-content-center'>
+          {isRightArrow && (
+            <ArrowButton onClickArrowButton={onClickRightArrowButton} direction={'right'} />
+          )}
+        </div>
+        <div className='flex p-5 overflow-hidden w-9/12'>
+          <div
+            className='flex transition-transform duration-700 ease-in-out space-x-2'
+            style={{ transform: `translateX(${transX}rem)` }}
+          >
+            {categoryList.map((item, index) => {
+              return (
+                <CategoryItem
+                  key={index}
+                  categoryIcon={item.categoryIcon}
+                  categoryName={item.categoryName}
+                />
+              )
+            })}
+          </div>
+        </div>
+        <div className='w-10 h-10 flex place-items-center place-content-center'>
+          {isLeftArrow && (
+            <ArrowButton onClickArrowButton={onClickLeftArrowButton} direction={'left'} />
+          )}
         </div>
         <FilterButton setIsFilterModal={setIsFilterModal} />
         {isFilterModal && <FilterComponent setIsFilterModal={setIsFilterModal} />}
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/MainCategory/CategoryItem.jsx
+++ src/components/MainCategory/CategoryItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-
 function CategoryItem({ categoryIcon, categoryName }) {
   return (
     <>
-      <button className='flex flex-col items-center justify-center p-2 w-20'>
-        <img src={categoryIcon} width='30' height='30' />
-
-        <div className='whitespace-nowrap text-xs'>{categoryName}</div>
+      <button className='group flex flex-col items-center justify-center p-2 w-20'>
+        <div className='w-[30px] h-[30px]'>
+          <img src={categoryIcon} />
+        </div>
+        <div className='whitespace-nowrap text-xs w-full justify-center'>{categoryName}</div>
+        <div className='w-10/12 h-0.5 bg-white mt-2 transition-color duration-300 group-hover:bg-gray-300'></div>
       </button>
     </>
   )
