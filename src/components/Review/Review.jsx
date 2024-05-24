49176e8502c7e20c4541f910caf4a7c4695c4870
IforCU <tmdgus0844@gmail.com>
2024-05-24 13:52:46 +0900

    Fix : 새로 추가된 JSON에 맞게 Review가 나올 수 있도록 로직 변경

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,16 +1,15 @@
 import ReviewHeader from './ReviewHeader'
 import ReviewContent from './ReviewContent'
 import ReviewMoreButton from './ReviewMoreButton'
-import ReviewData from '../../../public/json/ReviewData.json'
 import { useState } from 'react'
 
-const Review = () => {
+const Review = ({ reviewData }) => {
   const [moreReview, setMoreReview] = useState(false)
   return (
     <div>
       <hr className='my-4 border-gray-300' />
       <div className='grid grid-cols-1 lg:grid-cols-2 w-full '>
-        {ReviewData.map((comment, index) => {
+        {reviewData.map((comment, index) => {
           if (!moreReview && index > 5) return null
           return (
             <div key={index} className='bg-white p-4 px-2 mr-20'>
--- src/pages/Comment/Comment.jsx
+++ src/pages/Comment/Comment.jsx
@@ -1,13 +1,44 @@
 import GuestFavorite from '../../components/GuestFavorite/GuestFavorite.jsx'
 import ReviewMeta from '../../components/ReviewMeta/ReviewMeta.jsx'
 import Review from '../../components/Review/Review.jsx'
+import { useState, useEffect } from 'react'
 
 const Comment = () => {
+  const [reviewData, setReviewData] = useState([])
+
+  useEffect(() => {
+    fetch('/json/Review.json')
+      .then(response => response.json())
+      .then(data => {
+        setReviewData(processReviewData(data))
+      })
+      .catch(error => console.error('Error fetching ReviewData.json:', error))
+      .finally(console.log(reviewData))
+  }, [])
+
+  // ratings 평균 값을 내주는 함수
+  const calculateAverageRating = ratings => {
+    const totalScore = Object.values(ratings).reduce((acc, curr) => acc + curr, 0)
+    const numberOfCategories = Object.keys(ratings).length
+    return (totalScore / numberOfCategories).toFixed(1)
+  }
+
+  // rating 객체로 가공하는 함수.
+  const processReviewData = data => {
+    return data.map(item => {
+      const averageRating = calculateAverageRating(item.Ratings)
+      return {
+        ...item.review,
+        rating: parseFloat(averageRating), // 소수점 한 자리까지 표시
+      }
+    })
+  }
+
   return (
     <div className='px-20'>
       <GuestFavorite />
       <ReviewMeta />
-      <Review />
+      <Review reviewData={reviewData} />
     </div>
   )
 }
