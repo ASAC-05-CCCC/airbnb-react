99cf4c7852ad4d9ca21999b100aaadeb7a65bd6b
jueun0725 <kimjueun010@naver.com>
2024-05-24 16:50:48 +0900

    Feat: CardInfo 수정

--- src/components/PlaceList/PlaceCardInfo.jsx
+++ src/components/PlaceList/PlaceCardInfo.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 // 카드 정보 {지역,별점,거리,날짜,가격}
 const PlaceCardInfo = ({ id, location, rating, distance, dates, price }) => {
+  // price 숫자 변환
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(price)
+
   return (
     <div className='card_info pt-2'>
       <div className='card_header flex justify-between items-center'>
@@ -11,7 +14,7 @@ const PlaceCardInfo = ({ id, location, rating, distance, dates, price }) => {
       <div>
         <p className='text-zinc-600 text-sm font-normal '>{distance} 거리</p>
         <p className='text-zinc-600 text-sm font-normal'>{dates}</p>
-        <p className='text-neutral-800 text-sm'>{price}</p>
+        <p className='text-neutral-800 text-base font-medium'>\{formattedPrice}/박</p>
       </div>
     </div>
   )
