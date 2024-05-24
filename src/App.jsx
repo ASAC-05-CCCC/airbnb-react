2cc580f0f016f13f73467f6e1f661e48172f35c8
IforCU <tmdgus0844@gmail.com>
2024-05-24 10:21:23 +0900

    Fix : PR 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import Comment from './pages/Comment/Comment.jsx'
-
 function App() {
-  return (
-    <>
-<<<<<<< HEAD
-      <div>
-        <Comment />
-      </div>
-=======
-      <Comment />
->>>>>>> c197d2a94d694cbcd3608a297659d877db37ca98
-    </>
-  )
+  return <></>
 }
 
 export default App
