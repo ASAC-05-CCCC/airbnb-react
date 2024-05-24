aba079ad58c5f203cc8890dd9ad3e7e7b269aade
IforCU <tmdgus0844@gmail.com>
2024-05-24 13:51:29 +0900

    Fix : 이미지가 없을 시 defaultImage를 가지도록 수정

--- src/components/Review/ReviewHeader.jsx
+++ src/components/Review/ReviewHeader.jsx
@@ -1,6 +1,16 @@
+import DefaultProfileImage from '../../assets/DefaultProfileImage.svg'
+
 const ReviewHeader = ({ image, name, nationality }) => (
   <div className='flex items-center'>
-    <img src={image} alt={name} className='w-10 h-10 bg-neutral-200 rounded-full mr-4' />
+    <img
+      src={image}
+      alt={name}
+      className='w-10 h-10 bg-neutral-200 rounded-full mr-4'
+      onError={e => {
+        e.target.onError = null
+        e.target.src = DefaultProfileImage
+      }}
+    />
     <div>
       <p className='font-bold'>{name}</p>
       <p className='text-sm font-normal text-neutral-800'>{nationality}</p>
