54bbf22d3bc4591e06bcf7843f522ddfe03aa441
yunhyungNAM <iys04003@naver.com>
2024-05-27 23:47:33 +0900

    Fix: RoomIntroduce CSS for layout

--- src/components/RoomIntroduce/Translation.jsx
+++ src/components/RoomIntroduce/Translation.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Translation = () => {
   return (
-    <div className="max-w-2xl mx-auto p-4 border-t border-gray-300">
-      <div className='flex space-x-2 border items-end'>
+    <div className="w-full grid gap-2border-gray-300">
+      <div className='flex space-x-2 border items-end mt-4 mb-8'>
         <div>일부 정보는 자동 번역되었습니다.</div>
         <button className='underline'>원문 보기</button>
       </div>
